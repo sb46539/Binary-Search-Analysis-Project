316ea612bd6427e389ab0e28b760e0b1a7cd5e9c
sb46539 <sb46539@email.vccs.edu>
2024-11-03 00:30:38 -0400

    Random completed.

--- Binary Search Analysis Project/Binary Search Analysis Project.cpp
+++ Binary Search Analysis Project/Binary Search Analysis Project.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <random>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dist(1, 100);
+
+    for (int i = 0; i < 5; ++i) {
+        int randomNum = dist(gen);
+        std::cout << randomNum << std::endl;
+    }
+
     return 0;
 }
